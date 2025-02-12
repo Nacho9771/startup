356e1a0c8a4b3c8d1aebd776238d3bf6f7901547
Connor Gordon <cgordon0@byu.edu>
2025-02-11 23:33:38 -0700

    Installed all of the packages, including npm, bootstrap, react, and the other ones included in the simon tutorial. I also updated some of the code, just working on it bit by bit.

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './main.css';
+import './app.css';
 import './home/home.css';
 
 import Home from './home/home';
