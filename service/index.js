6339fd63c639197759f68e973da80f2fc4690a45
Connor Gordon <cgordon0@byu.edu>
2025-03-11 17:37:00 -0600

    I added the express module and other node modules to the service file...I hope it didnt just break it but we will see also *adding not assing in the previous commit message, oopsie

--- service/index.js
+++ service/index.js
@@ -1,3 +1,15 @@
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const express = require('express');
+const app = express();
 
 app.use(express.static('public'));
+
+// REAL BIG CODE
+
+app.get('*', (_req, res) => {
+    res.send({ msg: 'Easytrading service' });
+  });
+  
+app.listen(port, () => {
+console.log(`Listening on port ${port}`);
+});
